18a02dd1687d37299df0c2d8537ec3ebe0ded666
RahulR100 <rahulrao@email.com>
2021-01-10 18:46:35 -0500

    added divide and conquer function

--- main.m
+++ main.m
@@ -1,17 +1,20 @@
+%some constantsis
+numVars = 3;
+
 %import data from combined results file
 
-input_file = "input_data/Combined results.xlsx";
-
-rootdata  = xlsread(input_file, "B3:CS25");
-spandata  = xlsread(input_file, "B29:DI51");
-sweepdata = xlsread(input_file, "B55:DI77");
-tipdata   = xlsread(input_file, "B81:DY103");
-widthdata = xlsread(input_file, "B107:BM129");
+combinedData = importdat();
 
 %now analyse the data
 
 %find all instances for each mach val and aoa
 %then sort that data (now an array of arrays)
+
+for i = 1:length(combinedData)
+    individualData = struct2cell(combinedData{1, i});
+    combinedData{1, i} = divandconq(individualData{1}, numVars);
+end
+
 %then graph all results for all values (now an array of graphs)
 
-%and now graph it
+%and now save it
