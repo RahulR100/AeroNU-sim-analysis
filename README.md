5a4290a926e96b7dc9cd4e2c447da4d820f85984
RahulR100 <rahulrao@email.com>
2021-01-11 18:30:54 -0500

    basic folder creation for output

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # AeroNU-sim-analysis
+
 Analyse data after solidworks parateric sim analysis
+
+---
+### Usage guide:
+- Take simulation results (excel format only) and dump them in `input_data`
+- Open `main.m` and change `numVars` to the number of variables analysed in the parametric study
+- Run `main.m` and look for data in `output_data` with the current timestamp
--- main.m
+++ main.m
@@ -9,11 +9,24 @@ combinedData = importdat();
 
 for i = 1:length(combinedData)
     individualData = struct2cell(combinedData{1, i});
-    combinedData{1, i} = struct("name", individualData{1, 1}, "data", divandconq(individualData{2, 1}, numVars));
+    tableData = individualData{2, 1};
+    combinedData{1, i} = struct("name", individualData{1, 1}, "data", divandconq(individualData{2, 1}, numVars), "keys", {reshape(tableData{1:(numVars - 1), 1}, 1, [])});
 end
 
 %STEP 4: create basic output folder structure, prepare to graph
 
-%STEP 5: graph all results for all values (now an array of graphs)
+root = "output_data";
+currDate = strrep(datestr(datetime), ':', '_');
+%mkdir(append(root, currDate));
+
+for i = 1:length(combinedData)
+    %mkdir(append(root, currDate, combinedData{1, i}.name));
+    dat = combinedData{1, i}.data;
+    for j = 1:length(dat)
+        mkdir(append(root, "/", currDate, "/", combinedData{1, i}.name, "/", num2str(reshape(dat(j).head', 1, []))));
+    end
+end
+
+%STEP 5: graph all results for all values
 
 %STEP 6: now save everything to output_data
