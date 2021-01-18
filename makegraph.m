34b65fb698e9b9670483abfc7a231affb2000cf6
RahulR100 <rahulrao@email.com>
2021-01-17 22:05:04 -0500

    removed errant strrep

--- makegraph.m
+++ makegraph.m
@@ -9,7 +9,7 @@ for i = 2:height(data)
    
     f = gcf;
     plot(data{1, (2:width(data))}, data{i, (2:width(data))});
-    ylabel(strrep(data{i, 1}));
+    ylabel(data{i, 1});
     exportgraphics(f, append(location, "/", strrep(data{i, 1}, '/', '|'), ".png"));
     
     
