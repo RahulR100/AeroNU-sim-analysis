c86db8498070d007144d09cca0761760c8be852e
RahulR100 <rahulrao@email.com>
2021-02-06 19:04:32 -0500

    Reducer is now functional

--- reducer.m
+++ reducer.m
@@ -3,13 +3,30 @@
 
 function newResults = reducer(oldData, computedData)
     
-    sortedData = [];
+    sortedData = [[], struct("head", oldData{1, 1}(1), "data", [struct("key", oldData{1, 1}(2), "value", computedData(1))])];
+    count = 2;
     
-    for i = 1:width(oldData)
-        key = oldData{1, i};
-        head = key(1);
-        
-        disp(head);
-    end
+    for i = 2:length(oldData)
 
+        similar = false;
+        for j = 1:length(sortedData)           
+           
+           if isequal(sortedData(j).head, oldData{1, i}(1))
+               sortedData(j).data = [sortedData(j).data, struct("key", oldData{1, i}(2), "value", computedData(count))];
+               count = count + 1;
+               similar = true;
+               break;
+           end
+           
+        end
+        
+        if similar == false
+           sortedData = [sortedData, struct("head", oldData{1, i}(1), "data", [struct("key", oldData{1, i}(2), "value", computedData(count))])];
+           count = count + 1;
+        end
+        
+    end
+    
+    newResults = sortedData;
+    
 end
